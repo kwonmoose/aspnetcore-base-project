20251a161b61259b103183cfde032d5b8096bdde
kwonmoose <scrt4364@gmail.com>
2022-02-25 19:13:48 +0900

    주석 추가

--- src/application/Program.cs
+++ src/application/Program.cs
@@ -19,8 +19,12 @@ if (app.Environment.IsDevelopment())
 // CDN에서 http를 https로 리디렉션하기 때문에 주석 처리
 // app.UseHttpsRedirection();
 
+// 인증 미들웨어 - 사용자를 인증하는 미들웨어
+// https://docs.microsoft.com/ko-kr/aspnet/core/security/authentication/?view=aspnetcore-6.0
 app.UseAuthentication();
 
+// 권한 미들웨어 - 사용자에게 요청에 대해 권한이 있는지 확인하는 미들웨어
+// https://docs.microsoft.com/ko-kr/aspnet/core/security/authorization/introduction?view=aspnetcore-6.0
 app.UseAuthorization();
 
 app.MapControllers();
